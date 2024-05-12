eaad93585b9e5688bd2751e8d2bc91722dc2001a
manishradiansys <mkumar@radiansys.com>
2024-05-12 07:53:11 +0530

    feat: added auth functionality

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
 import connectMongoDB from "./db/connectMongoDB.js";
 import authRoutes from "./routes/auth.routes.js"
 
@@ -7,6 +9,11 @@ dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT || 5000 // 8000
+
+app.use(express.json()); // to parse req.body
+app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
+
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
