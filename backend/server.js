072f7b01cda3ddd19d438bea88a08537a6065f95
manishradiansys <mkumar@radiansys.com>
2024-05-20 20:18:40 +0530

    feat: added user routes

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,24 @@
 import express from 'express';
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import {v2 as cloudinary} from "cloudinary";
+
+
+import authRoutes from "./routes/auth.routes.js"
+import usersRoutes from "./routes/user.routes.js"
+
 
 import connectMongoDB from "./db/connectMongoDB.js";
-import authRoutes from "./routes/auth.routes.js"
 
 dotenv.config();
-const app = express();
 
+cloudinary.config({
+	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+	api_key: process.env.CLOUDINARY_API_KEY,
+	api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const app = express();
 const PORT = process.env.PORT || 5000 // 8000
 
 app.use(express.json()); // to parse req.body
@@ -16,6 +27,7 @@ app.use(express.urlencoded({ extended: true })); // to parse form data(urlencode
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/users", usersRoutes);
 
 app.listen(PORT, () => {
     connectMongoDB();
