0e9e65e315815cc0fb3aa549ab2af1c006d12cdc
Manish <manishraz32@gmail.com>
2024-06-06 14:56:05 +0530

    setup for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
 import { v2 as cloudinary } from "cloudinary";
 
 
@@ -22,6 +23,7 @@ cloudinary.config({
 
 const app = express();
 const PORT = process.env.PORT || 5000 // 8000
+const __dirname = path.resolve();
 
 app.use(express.json()); // to parse req.body
 app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
@@ -32,6 +34,14 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", usersRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
 
 app.listen(PORT, () => {
 	connectMongoDB();
